97ce434caa058a7d2ea81f1cc24a959ced1785b0
Mohammed Irshad Nirban <Mohammed Irshad Nirban>
2026-02-23 03:29:15 +0000

    Add server health monitoring script

---
+++ healthcheck.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#!/bin/bash
+echo "--- Server Health Report ---"
+date
+echo -e "\n1. Disk Usage:"
+df -h | grep '^/dev/'
+echo -e "\n2. Memory Usage:"
+free -h
+echo -e "\n3. System Uptime:"
+uptime
