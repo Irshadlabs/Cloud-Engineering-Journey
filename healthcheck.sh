a13d2782eacae2605d5346e0fa4d8e1e44acdec2
Irshadlabs <mohdi0365@gmail.com>
2026-02-24 02:49:48 +0000

    script updated to save log

--- healthcheck.sh
+++ healthcheck.sh
@@ -10,3 +10,13 @@ echo -e "\n3. System Uptime:"
 uptime
 script run succesfully
 # Updated on Mon Feb 23 03:55:12 UTC 2026 with correct email config
+#!/bin/bash
+# Log file path
+LOG_FILE="/home/irshad/my_cloud_scripts/server_usage.log"
+
+echo "----------------------------" >> $LOG_FILE
+date >> $LOG_FILE
+echo "Disk Usage:" >> $LOG_FILE
+df -h | grep '^/dev/' >> $LOG_FILE
+echo "Memory Usage:" >> $LOG_FILE
+free -h >> $LOG_FILE
