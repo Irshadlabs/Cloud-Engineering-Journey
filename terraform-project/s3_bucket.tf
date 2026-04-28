5e122a95debc603a47fd0b417345a59bef27bd29
Irshadlabs <mohdi0365@gmail.com>
2026-04-28 04:29:14 +0300

    fix: simplified s3_bucket script and removed duplicates

--- terraform-project/s3_bucket.tf
+++ terraform-project/s3_bucket.tf
@@ -5,13 +5,7 @@
 #              creation of a secure S3 Bucket for storage.
 # =========================================================
 
-# 1. Define the Cloud Provider (AWS)
-# This block tells Terraform to use the AWS plugin and sets the deployment region.
-provider "aws" {
-  region = "us-east-1"
-}
-
-# 2. Create an S3 Bucket Resource
+# 1. Create an S3 Bucket Resource
 # 'my_portfolio_bucket' is the internal name used by Terraform to track this resource.
 resource "aws_s3_bucket" "my_portfolio_bucket" {
   
@@ -27,7 +21,7 @@ resource "aws_s3_bucket" "my_portfolio_bucket" {
   }
 }
 
-# 3. Security Configuration: Public Access Block
+# 2. Security Configuration: Public Access Block
 # As an AWS Architect, I am implementing this to ensure the bucket is NOT public.
 # This follows the "Security Best Practices" to prevent accidental data leaks.
 resource "aws_s3_bucket_public_access_block" "security_block" {
